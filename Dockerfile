80a24e6976052e069d35334250195e545d021251
Johnny Graettinger <johnny.graettinger@liveramp.com>
2019-02-13 12:26:32 -0500

    build & test infrastructure
    - Dockerized build target.
    - Helm charts.
    - Test examples deployment script.
    - Gopkg.toml & lock
    - A first-pass, partially complete README.md

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM liveramp/gazette AS base
+
+FROM base AS build
+
+ARG DEP_VERSION=v0.5.0
+ARG IMPORT_PATH=git.liveramp.net/jgraet/factable
+
+RUN curl -fsSL -o /usr/local/bin/dep \
+    https://github.com/golang/dep/releases/download/${DEP_VERSION}/dep-linux-amd64 \
+ && chmod +x /usr/local/bin/dep
+
+COPY Gopkg.toml Gopkg.lock /go/src/${IMPORT_PATH}/
+RUN cd /go/src/${IMPORT_PATH}/ \
+ && dep ensure -vendor-only \
+ && rm -rf "$GOPATH/pkg/dep"
+
+# Copy, install, and test library and main packages.
+COPY pkg /go/src/${IMPORT_PATH}/pkg
+RUN go install -tags rocksdb -race ${IMPORT_PATH}/pkg/...
+RUN go test    -tags rocksdb -race ${IMPORT_PATH}/pkg/...
+RUN go install -tags rocksdb       ${IMPORT_PATH}/pkg/...
+
+COPY cmd /go/src/${IMPORT_PATH}/cmd
+RUN go install -tags rocksdb ${IMPORT_PATH}/cmd/...
+RUN go test    -tags rocksdb ${IMPORT_PATH}/cmd/...
+
+# Stage 4: Pluck factable binaries & plugins onto base.
+FROM base as factable
+
+COPY --from=build \
+    /go/bin/vtable \
+    /go/bin/factctl \
+    /go/bin/quotes-extractor \
+    /go/bin/quotes-publisher \
+    /go/bin/
---
+++ test/deploy_factable.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -Eeux -o pipefail
+
+readonly ROOT="$(CDPATH= cd "$(dirname "$0")/.." && pwd)"
+readonly USAGE="Usage: $0 kube-context kube-namespace optional-broker-namespace"
+readonly NAMESPACE="${2?Kubernetes namespace is required. ${USAGE}}"
+readonly BK_NAMESPACE="${3:-${NAMESPACE}}"
+
+. "${ROOT}/vendor/github.com/LiveRamp/gazette/v2/test/lib.sh"
+configure_environment "${1?Kubernetes context is required. ${USAGE}}"
+
+
+# GAZCTL runs gazctl in an ephemeral docker container which has direct access to
+# the kubernetes networking space. The alternative is to run gazctl on the host
+# and port-forward broker or consumer pods as required to expose the service (yuck).
+readonly GAZCTL="${DOCKER} run \
+  --rm \
+  --interactive \
+  --env BROKER_ADDRESS \
+  --env CONSUMER_ADDRESS \
+  liveramp/gazette \
+  gazctl"
+
+readonly FACTCTL="${DOCKER} run \
+  --rm \
+  --interactive \
+  --env BROKER_ADDRESS \
+  --env EXTRACTOR_ADDRESS \
+  --env VTABLE_ADDRESS \
+  liveramp/factable
+  factctl"
+
+
+# Create all journal fixtures. Use `sed` to replace the MINIO_RELEASE token with the
+# correct Minio service address.
+sed -e "s/MINIO_RELEASE/$(helm_release ${BK_NAMESPACE} minio).${BK_NAMESPACE}/g" ${ROOT}/test/quotes.journalspace.yaml | \
+  BROKER_ADDRESS=$(release_address $(helm_release ${BK_NAMESPACE} gazette) gazette) ${GAZCTL} journals apply --specs /dev/stdin
+
+# Install a test "gazette-zonemap" ConfigMap in the namespace, if one doesn't already exist.
+install_zonemap ${NAMESPACE}
+
+# Install the "factable" chart, first updating dependencies and blocking until release is complete.
+${HELM} dependency update ${ROOT}/charts/factable
+${HELM} install --namespace ${NAMESPACE} --wait ${ROOT}/charts/factable --values /dev/stdin << EOF
+extractor:
+  image:
+    binary: /go/bin/quotes-extractor
+
+global:
+  etcd:
+    endpoint: http://$(helm_release ${BK_NAMESPACE} etcd)-etcd.${BK_NAMESPACE}:2379
+  gazette:
+    endpoint: http://$(helm_release ${BK_NAMESPACE} gazette)-gazette.${BK_NAMESPACE}:80
+EOF
+
+# Load the "quotes" example schema.
+${DOCKER} run --rm --interactive liveramp/factable quotes-publisher write-schema | \
+  EXTRACTOR_ADDRESS=$(release_address $(helm_release ${NAMESPACE} factable) extractor) \
+  ${FACTCTL} schema update --instance $(helm_release ${NAMESPACE} factable) --revision 0 --format json --path /dev/stdin
+
+# go install git.liveramp.net/jgraet/factable/cmd/examples/quotes-publisher && ~/go/bin/quotes-publisher publish --quotes pkg/
